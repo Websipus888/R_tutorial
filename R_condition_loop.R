b8df54c22b0a927e3132ff1db7c512e0d23a3fd4
Web <web.vasutorn@gmail.com>
2021-01-21 16:22:50 +0700

    second commit

--- R_condition_loop.R
+++ R_condition_loop.R
@@ -1,1 +1,24 @@
 # Conditions and Loop
+# Control FLOW 1.if     2.For...loop    3.while...loop
+ 
+# If-Else
+# เขียนโดยใชข้การประกาศฟังก์ชั่น
+type_number <- function(x){
+    if (x %% 2 ==0){
+        print("This is EVEN number")
+    }else{
+        print("This is ODD number")
+}
+}
+
+# เขียนโดยใช้ฟังก์ชั่น ifelse
+a <- c(1:10)
+help(ifelse)
+ifelse (x %%2 ==0 ,"EVEN","ODD")
+
+# For (this is not stable for R)
+# iteration 
+x <- 1:10
+for (i in x){
+    print(i)
+}
